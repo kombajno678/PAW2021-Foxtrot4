ba6a17a52fba142a08e66b0f67221dfa44b148d1
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2020-10-17 20:13:17 +0200

    skeleton, get and put

---
+++ paw-foxtrot4/src/app/components/test/test.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { TestService } from 'src/app/services/test/test.service';
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+
+  list = [];
+
+
+  key:number;
+  value:string;
+
+
+
+  constructor(private testService:TestService) { }
+
+  ngOnInit(): void {
+    this.update();
+  }
+
+  update(){
+    this.testService.get().subscribe(r =>{
+      console.log('received data: ', r);
+      this.list = r;
+
+    })
+  }
+
+  onSendButtonClick(){
+    console.log(this.key, this.value);
+    this.testService.put([{key: this.key, value: this.value}]).subscribe(r =>{
+      console.log('sent', r);
+      this.update();
+    });
+
+  }
+
+}
---
+++ paw-foxtrot4/src/app/services/test/test.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+
+  private apiUrl = 'http://localhost:5000';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+
+
+
+  get():Observable<[]>{
+    /*
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('Accept', 'application/json');
+*/
+    let url = this.apiUrl + '/skeleton/get';
+
+    return this.http.get<[]>(url)
+    .pipe(
+      tap(_ => this.log('brr service working')),
+      catchError(this.handleError<[]>('get ' + url, []))
+    );
+
+  }
+
+  put(objects:any[]){
+    /*let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('Accept', 'application/json');
+*/
+    console.log(objects);
+    let url = this.apiUrl + '/skeleton/put';
+
+    return this.http.post<[]>(url, objects)
+    .pipe(
+      tap(_ => this.log('brr service working')),
+      catchError(this.handleError<[]>('post ' + url, []))
+    );
+  }
+
+
+
+
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  private log(message: string) {
+    console.log(`AccountService > ${message}`);
+  }
+
+
+
+
+
+
+
+
+
+}
