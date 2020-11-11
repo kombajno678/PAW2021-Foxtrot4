b2ef880ab58b90517cae6b4bb789db5f57afddf5
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2020-11-11 20:03:44 +0100

    allow tokens from web api on heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -28,7 +28,7 @@ import { LoadingSpinnerComponent } from './components/misc/loading-spinner/loadi
 import { CreateBoardDialogComponent } from './components/boards/create-board-dialog/create-board-dialog.component';
 
 
-function tokenGetter(){
+function tokenGetter() {
   return localStorage.getItem('user');
 }
 
@@ -56,15 +56,15 @@ function tokenGetter(){
     BrowserAnimationsModule,
     JwtModule.forRoot({
       config: {
-        tokenGetter : tokenGetter,
-        allowedDomains : ['localhost:5000'],
-        disallowedRoutes : [],
+        tokenGetter: tokenGetter,
+        allowedDomains: ['localhost:5000', 'https://foxtrot4-backend.herokuapp.com'],
+        disallowedRoutes: [],
 
       }
     }),
   ],
   providers: [
-    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
+    { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }
   ],
   bootstrap: [AppComponent]
 })
