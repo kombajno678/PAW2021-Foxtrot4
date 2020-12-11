4b6f60a320808c1fbbeeef8cd6d924895a820e00
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2020-12-11 13:08:43 +0100

    card due date works

--- src/app/components/dialogs/card/card.component.ts
+++ src/app/components/dialogs/card/card.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Board } from 'src/app/models/Board';
 import { BoardList } from 'src/app/models/BoardList';
-import {Comment } from '../../../models/Comment'
+import { Comment } from '../../../models/Comment'
 import { ListCard } from 'src/app/models/ListCard';
 import { BoardsService } from 'src/app/services/boards/boards.service';
 import { SnackbarService } from 'src/app/services/snackbar/snackbar.service';
@@ -18,7 +18,7 @@ export interface CardDialogData {
   list: BoardList;
   card: ListCard;
 }
-export interface Color{
+export interface Color {
   id: Number,
   bgcolor: String,
   choosed: Boolean
@@ -41,36 +41,43 @@ export class CardComponent implements OnInit {
   listOfComments: Comment[];
   changeDetectorRefs: ChangeDetectorRef;
   allColors: Color[];
+
   form: FormGroup;
   formComment: FormGroup;
+  formDueDate: FormGroup;
+
   displayedColumns: string[] = ['color'];
   colorOfCard: string;
 
 
   constructor(
     public dialogRef: MatDialogRef<CardComponent>,
-    
+
     @Inject(MAT_DIALOG_DATA) public data: CardDialogData,
     private service: BoardsService,
     private snackbar: SnackbarService) {
-    this.allColors  = [
-      {id: 1, bgcolor: "", choosed: true},
-      {id: 2, bgcolor: "#ff5757", choosed: false},
-      {id: 3, bgcolor: "#57dbff", choosed: false},
-      {id: 4, bgcolor: "#f5ff57", choosed: false},
-      {id: 5, bgcolor: "#76ff57", choosed: false},
+    this.allColors = [
+      { id: 1, bgcolor: "", choosed: true },
+      { id: 2, bgcolor: "#ff5757", choosed: false },
+      { id: 3, bgcolor: "#57dbff", choosed: false },
+      { id: 4, bgcolor: "#f5ff57", choosed: false },
+      { id: 5, bgcolor: "#76ff57", choosed: false },
     ]
- 
+
     this.displayedColumns = ["bgcolor"]
-    this.allComments= new BehaviorSubject<Comment[]>([]);
+    this.allComments = new BehaviorSubject<Comment[]>([]);
     this.board = data.board;
     this.list = data.list;
     this.card = data.card;
-    
- 
-  
-    
-    this.formComment  = new FormGroup({
+
+
+
+
+    this.formDueDate = new FormGroup({
+      due_date: new FormControl(this.card.due_date, [Validators.required]),
+      completed: new FormControl(this.card.completed ? this.card.completed : false, [Validators.required]),
+    })
+    this.formComment = new FormGroup({
       content: new FormControl(null, [Validators.required]),
     })
     this.form = new FormGroup({
@@ -79,71 +86,97 @@ export class CardComponent implements OnInit {
 
 
   }
-  
+
 
   ngOnInit(): void {
     this.dialogRef.beforeClosed()
-    this.allColors.find( r=> 
-     r.bgcolor == this.card.labels
-      
+    this.allColors.find(r =>
+      r.bgcolor == this.card.labels
+
     ).choosed = true;
-    this.allColors.find( r=> 
+    this.allColors.find(r =>
       r.bgcolor != this.card.labels
-       
-     ).choosed = false;
+
+    ).choosed = false;
     this.update();
   }
 
-  update(){
-      
+  update() {
+
     this.colorOfCard = this.card.labels;
-      this.service.getComments(this.card).subscribe(r => {
-        this.listOfComments = r;
-        this.allComments.next(this.listOfComments);
-        console.log(this.allComments);
-      })
-     
+    this.service.getComments(this.card).subscribe(r => {
+      this.listOfComments = r;
+      this.allComments.next(this.listOfComments);
+      console.log(this.allComments);
+    })
+
   }
-  saveColor(color: string){
+  saveColor(color: string) {
     console.log(color)
     this.service.updateColor(this.card, color).
-    subscribe(r => {
-      if(r){
-        this.card = r
-        this.colorOfCard = this.card.labels;
-        this.allColors.find( s=> 
-          s.bgcolor == r.labels
-           
-         ).choosed = true;
-        
-        this.allColors.filter( s=> 
-          s.bgcolor != r.labels
-           
-         ).forEach(element => {
-           element.choosed = false;
-         });
-        
+      subscribe(r => {
+        if (r) {
+          this.card = r
+          this.colorOfCard = this.card.labels;
+          this.allColors.find(s =>
+            s.bgcolor == r.labels
+
+          ).choosed = true;
+
+          this.allColors.filter(s =>
+            s.bgcolor != r.labels
+
+          ).forEach(element => {
+            element.choosed = false;
+          });
+
           console.log(this.allColors)
-      }
-    })
-  }
-  saveComment() {
-    if(this.formComment.valid){
-      this.service.addComment( new Comment(null, this.formComment.controls.content.value, null, this.card.id))
-      .subscribe(r => {
-        console.log(r)
-        if(r){
-          this.formComment.controls.content.setValue("");
-          this.snackbar.openSnackBar('Comment added!');
-          r.posted_at= new Date(r.posted_at)
-          this.listOfComments.push(r);
-          this.allComments.next(this.listOfComments);
-        }else{
-          this.snackbar.openSnackBar('Error while saving comment :(');
         }
       })
+  }
+  saveComment() {
+    if (this.formComment.valid) {
+      this.service.addComment(new Comment(null, this.formComment.controls.content.value, null, this.card.id))
+        .subscribe(r => {
+          console.log(r)
+          if (r) {
+            this.formComment.controls.content.setValue("");
+            this.snackbar.openSnackBar('Comment added!');
+            r.posted_at = new Date(r.posted_at)
+            this.listOfComments.push(r);
+            this.allComments.next(this.listOfComments);
+          } else {
+            this.snackbar.openSnackBar('Error while saving comment :(');
+          }
+        })
     }
   }
+
+
+  onFormDueDateSubmit() {
+    console.log("onFormDueDateSubmit");
+
+    if (this.formDueDate.valid) {
+
+
+      this.card.due_date = new Date(this.formDueDate.controls.due_date.value);
+      this.card.completed = this.formDueDate.controls.completed.value;
+
+      this.service.updateCard(this.board, this.list, this.card).subscribe(r => {
+        console.log("onFormDueDateSubmit result = ", r);
+        if (r) {
+          this.snackbar.openSnackBar('Due date saved!');
+        } else {
+          this.snackbar.openSnackBar('Error while saving date :(');
+        }
+      })
+
+    }
+
+
+  }
+
+
   onSave() {
     if (this.form.valid) {
 
--- src/app/models/ListCard.ts
+++ src/app/models/ListCard.ts
@@ -23,11 +23,23 @@ export class ListCard {
 
     //activities[]
 
-    //due_date:Date
+    due_date: Date | string;
+    completed: boolean;
 
     //cover
 
-    constructor(id: number, title: string, content: string, position: number, archived: boolean, list_id: number, comments: Comment[], labels: string) {
+    constructor(
+        id: number,
+        title: string,
+        content: string,
+        position: number,
+        archived: boolean,
+        list_id: number,
+        comments: Comment[],
+        labels: string,
+        due_date?: Date | string,
+        completed?: boolean,
+    ) {
         this.id = id;
         this.card_name = title;
         this.content = content;
@@ -36,6 +48,8 @@ export class ListCard {
         this.list_id = list_id;
         this.comments = comments;
         this.labels = labels;
+        this.due_date = due_date ? due_date : null;
+        this.completed = completed ? completed : null;
     }
 
 }
